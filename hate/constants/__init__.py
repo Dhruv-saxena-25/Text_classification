cb3eab8cb4202715ab394ab713af43a6354cbf74
Dhruv Saxena <saxena25dhruv@gmail.com>
2024-05-24 03:01:33 +0100

    constants

--- hate/constants/__init__.py
+++ hate/constants/__init__.py
@@ -8,7 +8,7 @@ from datetime import datetime
 
 
 
-# Common constants
+# Common Constants
 TIMESTAMP: str = datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
 ARTIFACTS_DIR =   os.path.join("artifacts", TIMESTAMP)
 ZIP_FILE_NAME = 'dataset.zip'
@@ -17,16 +17,16 @@ TWEET = 'tweet'
 BUCKET_NAME = 'hates-speech-project'
 SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
 
-# Data ingestion constants
+# Data Ingestion Constants
 DATA_INGESTION_ARTIFACTS_DIR = "DataIngestionArtifacts"
 DATA_INGESTION_IMBALANCE_DATA_DIR = "imbalanced_data.csv"
 DATA_INGESTION_RAW_DATA_DIR = "raw_data.csv"
 
-# Data Validation Constant
+# Data Validation Constants
 DATA_VALIDATION_DIR_NAME: str = "DataValidationArtifacts"
 DATA_VALIDATION_REPORT_FILE_NAME: str = "report.yaml"
 
-# Data Transformation Constant
+# Data Transformation Constants
 DATA_TRANSFORMATION_ARTIFACTS_DIR = 'DataTransformationArtifacts'
 TRANSFORMED_FILE_NAME = "final.csv"
 DATA_DIR = "data"
@@ -35,3 +35,6 @@ AXIS = 1
 INPLACE = True
 DROP_COLUMNS = ['count','hate_speech','offensive_language','neither']
 CLASS = 'class'
+
+
+# Model Training Constants
