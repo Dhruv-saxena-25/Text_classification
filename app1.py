5aac8af3f03f5ff4e2172b35a1339f5b9c5cce21
Dhruv Saxena <saxena25dhruv@gmail.com>
2024-05-28 12:11:27 +0100

    prediction App

---
+++ app1.py
@@ -0,0 +1,37 @@
+from flask import Flask,request,render_template
+import numpy as np
+import pandas as pd
+from flask_cors import CORS, cross_origin
+from sklearn.preprocessing import StandardScaler
+from hate.pipeline.prediction_pipeline import PredictionPipeline
+from hate.constants import *
+
+application=Flask(__name__)
+CORS(application)
+app=application
+
+## Route for a home page
+
+# @app.route('/')
+# def index():
+#     return render_template('home.html') 
+
+@app.route("/", methods=['GET'])
+@cross_origin()
+def home():
+    return render_template('home.html')
+
+
+
+@app.route('/predict',methods=['POST'])
+def predictRoute():
+    if request.method == 'POST':
+            # Extract the text input from the form
+            input_text = request.form['text']
+            obj = PredictionPipeline()
+            result = obj.run_pipeline(input_text)
+            return render_template('result.html', results=result)
+    
+
+if __name__=="__main__":
+    app.run(host=APP_HOST, port= APP_PORT, debug= True)     
--- demo.py
+++ demo.py
@@ -2,5 +2,12 @@ from hate.logger import logging
 from hate.exception import CustomException
 import sys
 from hate.configuration.gcloud_syncer import GCloudSync
-obj = GCloudSync()
-obj.sync_folder_from_gcloud("hates-speech-project", "dataset.zip", "download/dataset")
+from hate.pipeline.train_pipeline import TrainPipeline
+
+
+obj = TrainPipeline()
+obj.run_pipeline()
+
+# obj = GCloudSync()
+
+# obj.sync_folder_from_gcloud("hates-speech-project", "dataset.zip", "download/dataset")
